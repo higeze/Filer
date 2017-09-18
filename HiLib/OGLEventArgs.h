3a1a7eb286fef43782e9681c4f72f99473ad0b04
higeze <higeze@yahoo.co.jp>
2017-09-18 12:13:56 +0900

    del

--- HiLib/OGLEventArgs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class COGLRenderer;
-
-struct OGLPaintEventArgs
-{
-	COGLRenderer* RendererPtr;
-	OGLPaintEventArgs(COGLRenderer* rendererPtr):RendererPtr(rendererPtr){}
-	virtual ~OGLPaintEventArgs(){}
-};
