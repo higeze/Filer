fd19324abc81ca9dcee7a6ec231e8cb2e8efe10d
Yuichiro Hirai <kuunarayaraneba@yahoo.co.jp>
2024-06-08 15:48:33 +0900

    20240608 s

--- D2DTextBox/Textbox.cpp
+++ D2DTextBox/Textbox.cpp
@@ -53,7 +53,7 @@ CTextBox::CTextBox(
 	 
 	Text.subscribe([this](auto e)
 	{
-		UpdateAll();
+		//UpdateAll();
 		switch (e.action) {
 			case notify_container_changed_action::insert://new,null,idx,-1
 				GetTextStorePtr()->OnTextChange(e.new_starting_index, e.new_starting_index, e.new_starting_index + e.new_items.size());
@@ -69,7 +69,8 @@ CTextBox::CTextBox(
 				break;
 			case notify_container_changed_action::reset://new,old,0,0
 				GetTextStorePtr()->OnTextChange(0, 0, 0);
-				MoveCaret(0, CPointF(0, GetLineHeight() * 0.5f));
+				//TODOTODO
+				MoveCaret(0, CPointF(0.f, 0.f));
 				break;
 			default:
 				break;
